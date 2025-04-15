2e15395a7e84a8a3c0161fd6a9c1c6e45f79e78c
Morteza <mortezabaghfar1382@gmail.com>
2025-04-15 15:22:19 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# ![Manage Property](<images/svg/Group 1.svg>)Manage Property
+
+## In This Project I Used 'HTML', 'CSS' and 'JS'.
+
+<p>This Project Has 'Full Responsive' For Mobile, Tablet And Laptop</p>
+
+<p>You Can See My Online Project</p>
+
+<a href="file:///C:/Users/MORTEZA/Desktop/practice/index.html">Online Project Manage Property</a>
---
+++ script/app.js
@@ -0,0 +1,18 @@
+let input = document.getElementById("#toggle-nav");
+
+let linetop = document.querySelector(".line-top");
+
+let linecenter = document.querySelector(".line-center");
+
+let linebottom = document.querySelector(".line-bottom");
+
+function test() {
+    linetop.classList.toggle("line1");
+    linecenter.classList.toggle("line2");
+    linebottom.classList.toggle("line3");
+}
+
+
+
+
+
